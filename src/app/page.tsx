4715c4c877688a6cad380fa15c6ba5734c618d2d
adricampo <adribdn7@gmail.com>
2026-04-02 14:23:12 +0200

    last fixes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function LandingPage() {
   }
 
   return (
-    <main className="relative h-screen w-full overflow-hidden bg-navy">
+    <main className="relative h-screen h-[100dvh] w-full overflow-hidden bg-navy">
       {heroImageUrl && (
         <Image
           src={heroImageUrl}
