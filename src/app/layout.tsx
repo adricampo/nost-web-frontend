b63ab3140aa2f7a21c2c8f2a730fb3c94304effa
adricampo <adribdn7@gmail.com>
2026-04-03 20:06:13 +0200

    fixed safari bg issue and reduced strapi calls to update data in around 5 mins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="theme-color" content="#13136B" />
+      </head>
       <body className={`${stanley.variable} ${apercu.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
