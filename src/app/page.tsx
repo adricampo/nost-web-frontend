c0847b7d6c8d4e9fe360e30999c98c0be73bf43f
adricampo <adribdn7@gmail.com>
2026-04-04 19:28:37 +0200

    trying to fix safari resolution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,11 +48,10 @@ export default async function LandingPage() {
       <div
         style={{
           top: 'calc(-1 * env(safe-area-inset-top, 0px))',
-          bottom: 'calc(-1 * env(safe-area-inset-bottom, 0px))',
           paddingLeft: 80,
           paddingRight: 36,
           backgroundColor: '#F5F4F1',
-          paddingBottom: 'calc(env(safe-area-inset-bottom, 0px) + 64px)'
+          paddingBottom: 'calc(env(safe-area-inset-bottom, 0px) + 200px)'
         }}
         aria-hidden="true"
       />
